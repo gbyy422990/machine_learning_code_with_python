affe96afb159740001c4adb0775882073d6d279c
bin gao <bin.gao@outlook.com>
2020-04-11 15:55:46 +0800

    PCA

---
+++ PCA/PCA.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# coding=utf8
+"""
+# Author: gaobin
+# Created Time : 2020-04-11 10:43:53
+
+# File Name: PCA.py
+# Description:
+
+"""
+
+import os, sys
+import numpy as np
+import matplotlib
+
+print("hello world!")
+
+
